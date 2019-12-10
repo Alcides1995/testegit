7f857a474e89c3a574e5b03518cd2d8f8b8186e2
Alcides1995 <58724804+Alcides1995@users.noreply.github.com>
2019-12-10 07:32:12 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
  res.status(200).send({ message: 'Olá mundo estou funcinando!' });
 })
 
+app.get('/Olá', (req, res) => {
+ res.status(200).send({ message: 'eu Sou o olá!' });
+})
+
 app.listen(3001, () => {
   console.log('Api rodando na porta 3001');
 })
